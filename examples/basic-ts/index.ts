aad8aa3ad82af0a8b5c8b7602a9dc52313d80975
Nieky Allen <nieky.allen@gmail.com>
2020-08-21 17:06:56 -0500

    update example

--- examples/basic-ts/index.ts
+++ examples/basic-ts/index.ts
@@ -1,6 +1,6 @@
-import PayPalClient from "paymigo";
+import PaymigoClient from 'paymigo';
 
-const client = new PayPalClient({
+const client = new PaymigoClient({
     mode: 'sandbox',
     clientId: 'CLIENT_ID',
     secretKey: 'SECRET_KEY',
