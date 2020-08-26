e297f70ab718110e8fa47224c337b267c3bca543
Nieky Allen <nieky.allen@gmail.com>
2020-08-26 13:29:47 -0500

    add more methods to OrdersClient

--- src/lib/orders.ts
+++ src/lib/orders.ts
@@ -1,4 +1,3 @@
-import { DisputesGetRequestParams } from 'legacy/DisputesGetRequest';
 import { BasePayPalClient } from 'types/paypal';
 // eslint-disable-next-line @typescript-eslint/no-var-requires
 const paypal = require('@paypal/checkout-server-sdk');
@@ -10,7 +9,70 @@ export class OrdersClient {
         this._client = client;
     }
 
-    public async list(params: DisputesGetRequestParams): Promise<any[]> {
+    // TODO input types
+    public async create(input: any) {
+        const request = new paypal.orders.OrdersCreateRequest();
+        request.requestBody(input);
+
+        try {
+            const response = await this._client.execute(request);
+            return response.result;
+        } catch (err) {
+            // TODO decide on error handling
+            console.error(err);
+        }
+    }
+
+    public async capture(orderId: string) {
+        const request = new paypal.orders.OrdersCaptureRequest(orderId);
+        request.requestBody({});
+
+        try {
+            const response = await this._client.execute(request);
+            return response.result;
+        } catch (err) {
+            // TODO decide on error handling
+            console.error(err);
+        }
+    }
+
+    /**
+     * uses a unique PayPal Order ID to retrieve order details to validate status and amount charged
+     * @param {String} orderId
+     * @param {Number} expectedAmount
+     */
+    public async verify(orderId: string, expectedAmount: number) {
+        const order = await this.getById(orderId);
+
+        if (
+            order.status !== 'COMPLETED' ||
+            order.purchase_units[0].amount.value != expectedAmount
+        )
+            throw new Error('Order has not been completed');
+
+        return true;
+    }
+
+    public async getById(orderId: string) {
+        const invalidIdError = new Error('Invalid order ID provided');
+        const request = new paypal.orders.OrdersGetRequest(orderId);
+        let order;
+
+        try {
+            const response = await this._client.execute(request);
+            order = response.result;
+        } catch (err) {
+            // TODO decide on error handling
+            console.error(err);
+            throw invalidIdError;
+        }
+
+        if (!order) throw invalidIdError;
+
+        return order;
+    }
+
+    public async list(params: any): Promise<any[]> {
         return [] as any[];
     }
 }
