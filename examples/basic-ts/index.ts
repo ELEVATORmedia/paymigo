7e7e1a272ffe7ec33f58fb665d79973c0d84edcb
Nieky Allen <nieky.allen@gmail.com>
2021-11-22 17:12:45 -0600

    update examples

--- examples/basic-ts/index.ts
+++ examples/basic-ts/index.ts
@@ -1,7 +1,15 @@
-import PaymigoClient from 'paymigo';
+import PaymigoClient from '@elevatormedia/paymigo';
 
 const client = new PaymigoClient({
     mode: 'sandbox',
     clientId: 'CLIENT_ID',
     secretKey: 'SECRET_KEY',
 });
+
+client.orders.capture('');
+
+client.payments.captureAuthorization('');
+
+client.disputes.getById('123abc');
+
+client.orders.update('123', { op: 'add', path: '' });
